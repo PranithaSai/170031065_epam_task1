a06c9856ea6e1f78548c5e206ef828bc365db647
Pranitha <Pranitha@LAPTOP-3O8A24HK>
2020-03-13 22:03:34 +0530

    commit

---
+++ epam_task/src/main/java/epam_task/Main.java
@@ -0,0 +1,16 @@
+package epam_task;
+
+public class Main {
+	public static void main(String []args)
+	{
+	int same;
+	child c=new child();
+	same=c.same;
+	System.out.println(same);
+	}
+}
+	 class child{
+	int same=989;
+	}
+
+
